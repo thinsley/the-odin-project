a99ffd76bdc01475786a92d3164ec8426cfb0745
Tristan Hinsley <thinsley@protonmail.com>
2022-06-16 19:30:20 -0400

    Added initial round functions and computer move generation

--- foundations/rock-paper-scissors/rock-paper-scissors.js
+++ foundations/rock-paper-scissors/rock-paper-scissors.js
@@ -1,4 +1,30 @@
-console.log("Welcome to Rock, Paper, Scissors!");
-let userInput = prompt("Please enter your move: ");
+//Function to start a new round. Prompts the use for their move and calls the function to generate a computer move.
+function playRound() {
+    console.log("Welcome to a new round!");
+    let userInput = prompt("Please enter your move: ").toLowerCase();
+    let computerMove = computerPlay();
 
-console.log(userInput);
+    //The logic that decides a winner
+
+}
+
+//Function that generates the computer's move
+function computerPlay() {
+    computerMoveNumber = Math.floor(Math.random()*3) + 1;
+    // console.log(computerMoveNumber);
+
+    let computerMove;
+    switch(computerMoveNumber) {
+        case 1: computerMove = "rock"; break;
+        case 2: computerMove = "paper"; break;
+        case 3: computerMove = "scissors"; break;
+    }
+    // console.log(computerMove);
+    return computerMove;
+}
+
+console.log("Welcome to Rock, Paper, Scissors!");
+console.log("")
+playRound();
+
+//console.log(userInput);
