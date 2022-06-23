5ff63e057277c524bf43e0c6b2b09fb81ed50d36
Tristan Hinsley <thinsley@protonmail.com>
2022-06-23 17:35:39 -0400

    Refactored code and cleaned up comments

--- foundations/rock-paper-scissors/rock-paper-scissors.js
+++ foundations/rock-paper-scissors/rock-paper-scissors.js
@@ -2,10 +2,7 @@
 function playGame() {
     let userScore = 0;
     let computerScore = 0;
-    let roundCounter = 1;
-    let roundScores;
     let userWinState;
-    let gameState = true;
 
     const divContainer = document.querySelector('#resultsDiv');
     const roundResultElement = document.createElement('div');
@@ -15,61 +12,57 @@ function playGame() {
     // console.log(gameButtons);
     gameButtons.forEach((button) => {
         button.addEventListener('click', () => {
-                userWinState = playRound(button.id);
-                if(userWinState == "true") {
-                    userScore++;
-                    roundResultElement.textContent = "Congratulations, you won the round! The score is now " + userScore + ":" + computerScore;
-                    // console.log("Congratulations, you won! Your " + userInput + " beats their " + computerMove + "!");
-                    if(userScore == 3) {
-                        gameResultElement.textContent = "You win! You had " + userScore + " wins while your opponent had "+ computerScore + ".";
-                        divContainer.appendChild(gameResultElement);
-                    }
+            userWinState = playRound(button.id);
+            //Legwork of the win condition logic
+            if(userWinState == "true") {
+                userScore++;
+                roundResultElement.textContent = "Congratulations, you won the round! The score is now " + userScore + ":" + computerScore;
+                // inner if statement checks for a game-ending round win
+                if(userScore == 3) {
+                    gameResultElement.textContent = "You win! You had " + userScore + " wins while your opponent had "+ computerScore + ".";
+                    divContainer.appendChild(gameResultElement);
                 }
-                else if(userWinState == "tie") {
-                    userScore++;
-                    computerScore++;
-                    roundResultElement.textContent = "It's a tie! The score is now " + userScore + ":" + computerScore;
-                    // console.log("Unfortunately, it was a tie! You both chose " + userInput);
-                    if(userScore == 3 && computerScore == 3) {
-                        gameResultElement.textContent = "Unfortunately, you lost. You had " + userScore + " wins while your opponent had "+ computerScore + ".";
-                        divContainer.appendChild(gameResultElement);
-                    }
-                    else if(userScore == 3) {
-                        gameResultElement.textContent = "You win! You had " + userScore + " wins while your opponent had "+ computerScore + ".";
-                        divContainer.appendChild(gameResultElement);
-                    }
-                    else if(computerScore == 3) {
-                        gameResultElement.textContent = "Unfortunately, you lost. You had " + userScore + " wins while your opponent had "+ computerScore + ".";
-                        divContainer.appendChild(gameResultElement);
-                    }
+            }
+            else if(userWinState == "tie") {
+                userScore++;
+                computerScore++;
+                roundResultElement.textContent = "It's a tie! The score is now " + userScore + ":" + computerScore;
+                // Inner if statement checks for a game-ending round tie
+                if(userScore == 3 && computerScore == 3) {
+                    gameResultElement.textContent = "Unfortunately, you lost. You had " + userScore + " wins while your opponent had "+ computerScore + ".";
+                    divContainer.appendChild(gameResultElement);
                 }
-                else {
-                    computerScore++;
-                    roundResultElement.textContent = "Bummer, you lost the round. The score is now " + userScore + ":" + computerScore;
-                    // console.log("Bummer, you lost. Their " + computerMove + " beats your " + userInput)
-                    
-                    if(computerScore == 3) {
-                        gameResultElement.textContent = "Unfortunately, you lost. You had " + userScore + " wins while your opponent had "+ computerScore + ".";
-                        divContainer.appendChild(gameResultElement);
-                    }
+                else if(userScore == 3) {
+                    gameResultElement.textContent = "You win! You had " + userScore + " wins while your opponent had "+ computerScore + ".";
+                    divContainer.appendChild(gameResultElement);
                 }
-                divContainer.appendChild(roundResultElement);
-            // }
+                else if(computerScore == 3) {
+                    gameResultElement.textContent = "Unfortunately, you lost. You had " + userScore + " wins while your opponent had "+ computerScore + ".";
+                    divContainer.appendChild(gameResultElement);
+                }
+            }
+            else {
+                computerScore++;
+                roundResultElement.textContent = "Bummer, you lost the round. The score is now " + userScore + ":" + computerScore;
+                
+                // inner if statement that checks for a computer win
+                if(computerScore == 3) {
+                    gameResultElement.textContent = "Unfortunately, you lost. You had " + userScore + " wins while your opponent had "+ computerScore + ".";
+                    divContainer.appendChild(gameResultElement);
+                }
+            }
+            divContainer.appendChild(roundResultElement);
         });
         //Learning Note: When you pass a function directly to eventLisener, it executes the function immediately.
     });
 }
 
-//Function to start a new round. Prompts the use for their move and calls the function to generate a computer move.
+//Function to start a new round. Basically just calls the computer move function and determines the round winner.
 function playRound(userInput) {
-    // let scores = [userScore, computerScore]
-    // console.log("Welcome to round " + roundCounter + "!");
-    // const userInput = prompt("Please enter your move: ").toLowerCase();
+
     const computerMove = computerPlay();
 
-    //console.log(computerMove);
-
-    //The logic that decides a winner
+    //The logic that decides a round winner
     let userWinState;
     let tieState = false;
     if(userInput == "rock" && computerMove == "scissors") userWinState = "true";
@@ -80,33 +73,12 @@ function playRound(userInput) {
     }
     else userWinState = "false";
 
-    // if(userWinState == true) {
-    //     // scores[0]++;
-    //     roundResultElement.textContent = "Congratulations, you won! Your " + userInput + " beats their " + computerMove + "!";
-    //     // console.log("Congratulations, you won! Your " + userInput + " beats their " + computerMove + "!");
-    // }
-    // else if(tieState == true) {
-    //     // scores[0]++;
-    //     // scores[1]++;
-    //     roundResultElement.textContent = "Unfortunately, it was a tie! You both chose " + userInput;
-    //     // console.log("Unfortunately, it was a tie! You both chose " + userInput);
-    // }
-    // else {
-    //     // scores[1]++;
-    //     roundResultElement.textContent = "Bummer, you lost. Their " + computerMove + " beats your " + userInput;
-    //     // console.log("Bummer, you lost. Their " + computerMove + " beats your " + userInput)
-    // }
-
-    // console.log("Your score is "+ scores[0] + ", and your opponent's score is " + scores[1] + ".");
-    // console.log("------------------------------------");
     return userWinState;
-
 }
 
 //Function that generates the computer's move
 function computerPlay() {
     computerMoveNumber = Math.floor(Math.random()*3) + 1;
-    // console.log(computerMoveNumber);
 
     let computerMove;
     switch(computerMoveNumber) {
@@ -114,16 +86,8 @@ function computerPlay() {
         case 2: computerMove = "paper"; break;
         case 3: computerMove = "scissors"; break;
     }
-    // console.log(computerMove);
+
     return computerMove;
 }
 
-//Welcome messages
-// console.log("Welcome to Rock, Paper, Scissors!");
-// console.log("------------------------------------")
-// console.log("This will be a best of 5 game. First to 3 wins is the winner.")
-
-//Main game call
 playGame();
-//console.log("Thank you for playing the game. See you next time!");
-//console.log(userInput);
