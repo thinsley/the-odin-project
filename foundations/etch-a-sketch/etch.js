425b7fd5679784facf6c5fe76400a5381bcba5aa
Tristan Hinsley <41385994+thinsley@users.noreply.github.com>
2022-06-25 11:03:37 -0400

    Add hover logic and allow user to define grid size

--- foundations/etch-a-sketch/etch.js
+++ foundations/etch-a-sketch/etch.js
@@ -1,18 +1,43 @@
-//Define parent container
-const papaContainer = document.querySelector("#papa-container");
-
-//Create 16x16 grid
-for (let i=0; i<256; i++) {
-    const childDiv = document.createElement("div");
-    childDiv.textContent = i;
-    childDiv.classList.add("child");
-    papaContainer.appendChild(childDiv);
+function removeGrid() {
+    const oldList = document.querySelectorAll(".child");
+    oldList.forEach((div) => {
+        papaContainer.removeChild(div);        
+    });
 }
 
-//Hover logic
-const childList = document.querySelectorAll(".child");
-childList.forEach((div) => {
-    div.addEventListener('mouseover', () => {
-        div.classList.add("hover")
-    });
+//Define parent container
+const papaContainer = document.querySelector("#papa-container");
+const gridButton = document.querySelector(".newGrid");
+let gridFactor;
+gridButton.addEventListener('click', () => {
+    try {
+        gridFactor = prompt('Enter the number of squares you want on each side');
+        if(gridFactor >= 31) {
+            throw("Grid length cannot exceed 30");
+        }
+        else {
+            //Remove old grid
+            removeGrid();
+            //Create grid based on user input
+            for (let i=0; i<(gridFactor*gridFactor); i++) {
+                const childDiv = document.createElement("div");
+                childDiv.textContent = i;
+                childDiv.classList.add("child");
+                childDiv.style.flexBasis = 100/gridFactor + "%";
+                papaContainer.appendChild(childDiv);
+
+                //Hover logic
+                const childList = document.querySelectorAll(".child");
+                childList.forEach((div) => {
+                    div.addEventListener('mouseover', () => {
+                        div.classList.add("hover");
+                    });
+                });
+            }
+        }
+    }
+    catch (e) {
+        alert("Error: " + e);
+        gridFactor = 16;
+    }
 });
